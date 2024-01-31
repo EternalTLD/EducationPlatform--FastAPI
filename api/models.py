d799a47e7df1581d1fa10250f0da59dd68858c14
EternalTLD <shvejkin01@mail.ru>
2024-01-31 20:59:19 +0300

    Added CRUD models

--- api/models.py
+++ api/models.py
@@ -1,9 +1,11 @@
 """Project models"""
 import re
 import uuid
+import datetime
+from typing import Optional
 
 from fastapi import HTTPException
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, constr
 
 
 LETTERS_PATTERN = re.compile(r"[a-zA-Zа-яА-Я\-]+$")
@@ -11,7 +13,7 @@ LETTERS_PATTERN = re.compile(r"[a-zA-Zа-яА-Я\-]+$")
 
 class TunedModel(BaseModel):
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class ShowUser(TunedModel):
@@ -20,12 +22,41 @@ class ShowUser(TunedModel):
     last_name: str
     email: EmailStr
     is_active: bool
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
 
 
-class UserCreate(BaseModel):
+class DeleteUser(TunedModel):
+    id: uuid.UUID
+    is_active: bool
+
+
+class CreateUser(TunedModel):
     first_name: str
     last_name: str
     email: EmailStr
+
+    @field_validator("first_name")
+    def validate_first_name(value):
+        if not LETTERS_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="First name should contains only letters"
+            )
+        return value
+
+    @field_validator("last_name")
+    def validate_last_name(value):
+        if not LETTERS_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Last name should contains only letters"
+            )
+        return value
+
+
+class UpdateUser(TunedModel):
+    first_name: Optional[constr(min_length=1)] = None
+    last_name: Optional[constr(min_length=1)] = None
+    email: Optional[EmailStr] = None
 
     @field_validator("first_name")
     def validate_first_name(value):
