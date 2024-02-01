308233ecb2913c7776bce2d596d6b85a3dde6f8a
EternalTLD <shvejkin01@mail.ru>
2024-02-01 20:14:50 +0300

    Added token settings

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DBSettings(BaseSettings):
-    DB_HOST: int
+    DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
@@ -11,9 +11,15 @@ class DBSettings(BaseSettings):
 
     @property
     def DATABASE_URL(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@0.0.0.0:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 db_settings = DBSettings()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+SECRET_KEY = "somesecretekey"
+
+ALGORITHM = "HS256"
