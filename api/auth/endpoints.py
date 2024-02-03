fd6f09581bbfba232812becf7d984a141700605f
EternalTLD <shvejkin01@mail.ru>
2024-02-03 22:16:51 +0300

    Refactored auth endpoints

--- api/auth/endpoints.py
+++ api/auth/endpoints.py
@@ -1,39 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from sqlalchemy.ext.asyncio import AsyncSession
 from jose import JWTError, jwt
 
 
 from config.settings import JWTSettings
-from db.models import User
-from db.session import get_db_session
-from db.dals import UserDAL
-from api.schemas.auth import Token, SingUp
-from api.schemas.users import UserResponse
-from .utils import Hasher, create_access_token
+from models.users import User
+from crud.users import UserCRUD
+from schemas.auth import Token, SingUp
+from schemas.users import UserResponse
+from utils.token import create_access_token
+from utils.hasher import Hasher
 
 auth_router = APIRouter()
 
 
-async def authenticate_user(
-    email: str, password: str, session: AsyncSession
-) -> User | None:
-    user_dal = UserDAL(session)
-    user = await user_dal.get_user_by_email(email)
-    if user is None or not Hasher.verify_password(password, user.hashed_password):
+@auth_router.post("/login", response_model=Token)
+async def login(
+    user_crud: UserCRUD, form_data: OAuth2PasswordRequestForm = Depends()
+) -> Token:
+    user = await user_crud.authenticate(form_data.username, form_data.password)
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Incorrect email or password",
         )
-    return user
 
-
-@auth_router.post("/login", response_model=Token)
-async def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
-    session: AsyncSession = Depends(get_db_session),
-) -> Token:
-    user = await authenticate_user(form_data.username, form_data.password, session)
     access_token = create_access_token(user.email)
     return Token(access_token=access_token, token_type="bearer")
 
@@ -42,9 +33,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 async def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    session: AsyncSession = Depends(get_db_session),
-):
+    user_crud: UserCRUD, token: str = Depends(oauth2_scheme)
+) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -60,8 +50,7 @@ async def get_current_user(
     except JWTError:
         raise credentials_exception
 
-    user_dal = UserDAL(session)
-    user = await user_dal.get_user_by_email(email)
+    user = await user_crud.get_by_email(email)
     if user is None:
         raise credentials_exception
 
@@ -69,20 +58,18 @@ async def get_current_user(
 
 
 @auth_router.post("/signup", response_model=UserResponse)
-async def sing_up(
-    data: SingUp, session: AsyncSession = Depends(get_db_session)
-) -> UserResponse:
-    user = await UserDAL(session).get_user_by_email(data.email)
+async def sing_up(data: SingUp, user_crud: UserCRUD) -> UserResponse:
+    user = await user_crud.get_by_email(data.email)
     if user is not None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"User with email {data.email} has already exists.",
         )
 
-    new_user = await UserDAL(session).create_user(
+    new_user = await user_crud.create(
         first_name=data.first_name,
         last_name=data.last_name,
         email=data.email,
-        password=Hasher.get_hash_password(data.password),
+        hashed_password=Hasher.get_hash_password(data.password),
     )
     return new_user
