e36eebf21664efc8626c9882917fd62cd789860d
EternalTLD <shvejkin01@mail.ru>
2024-02-05 22:48:15 +0300

    Added id field to base model

--- models/base.py
+++ models/base.py
@@ -1,5 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase
+import uuid
+
+from sqlalchemy import types
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class BaseModel(DeclarativeBase):
-    pass
+    id: Mapped[uuid.UUID] = mapped_column(
+        types.Uuid, primary_key=True, default=uuid.uuid4
+    )
